0e247ff5de684f5919c74c2f5fe487f699747395
Dakota Clark <dakota@allclarks.com>
2014-09-22 14:08:03 -0700

    Inital commit. ChangePasswordAtLogon is not yet working.

---
+++ Reset-ADAccountPassword.psm1
@@ -0,0 +1,34 @@
+﻿function Reset-ADAccountPassword
+{
+<#
+    .SYNOPSIS
+    Resets an AD user's password.
+    .DESCRIPTION
+    Resets an AD user's password.
+    .EXAMPLE
+    Reset-ADAccountPassword -User kjotaro -Password St@rDu$t86
+#>
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$True, Position=0)]
+        [alias("Account")]
+        [alias("Name")]
+        [string]$User,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$True, Position=1)]
+        [string]$Password,
+        [Parameter(Mandatory=$false)]
+        [alias("ResetAtLogon")]
+        [alias("ChangeAtLogon")]
+        [switch]$ChangePasswordAtLogon
+    )
+    if($ChangePasswordAtLogon -eq $true)
+    {
+        Set-ADAccountPassword -Identity $User -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force) -PassThru |
+            Set-ADuser -ChangePasswordAtLogon $true
+    }
+    else
+    {
+        Set-ADAccountPassword -Identity $User -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force)
+    }
+}
