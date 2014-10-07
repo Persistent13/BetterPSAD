58c38580e6f3eb0b36f2700de76cec71268fa623
Dakota Clark <dakota@allclarks.com>
2014-10-07 11:43:22 -0700

    Reset-ADAccountPassword can now accept arrays and output has been changed to be a proper format.

--- BetterPSAD.psm1
+++ BetterPSAD.psm1
@@ -11,20 +11,53 @@
     .EXAMPLE
     Reset-ADAccountPassword -User kjotaro -Password St@rDu$t86
 
+    User                                                           New Password
+    ----                                                           ------------
+    kjotaro                                                        St@rDu$t86
+
     This will reset the user kjotaro's password to St@rDu$t86 however the password
     will not be required to be changed at logon.
 
     .EXAMPLE
     C:\PS> Reset-ADAccountPassword -User kjotaro -Password St@rDu$t86 -ChangePasswordAtLogon
 
+    User                                                           New Password
+    ----                                                           ------------
+    kjotaro                                                        St@rDu$t86
+
     This will reset the user kjotaro's password to St@rDu$t86 and set the account
-    to have the password reset at next logon.
+    to have the password changed at next logon.
 
     .EXAMPLE
     C:\PS> Reset-ADAccountPassword -User kjotaro
-    The password is now: h1HZwOag
+
+    User                                                           New Password
+    ----                                                           ------------
+    kjotaro                                                        h1HZwOag
 
     When a password is not specified a password will be generated for the account.
+
+    .EXAMPLE
+    C:\PS> Reset-ADAccountPassword -User kjotaro, jjoestar -Password St@rDu$t86 -ChangePasswordAtLogon
+
+    User                                                           New Password
+    ----                                                           ------------
+    kjotaro                                                        St@rDu$t86
+    jjoestar                                                       St@rDu$t86
+
+    This command will reset all users to the password St@rDu$t86 and set the
+    accounts to have their passwords changed at logon.
+
+    .EXAMPLE
+    C:\PS> Reset-ADAccountPassword -User kjotaro, jjoestar
+
+    User                                                           New Password
+    ----                                                           ------------
+    kjotaro                                                        iLZt9NGh
+    jjoestar                                                       TjI9WvUF
+
+    When a password is not specified a unique password will be generated for each
+    of the accounts.
 #>
     [cmdletbinding()]
     param
@@ -34,7 +67,7 @@
                     Position=0,
                     ValueFromPipelinebyPropertyName=$True)]
         [Alias("Account","Name","UserName")]
-        [String]$User,
+        [String[]]$User,
 
         [Parameter(Mandatory=$false, Position=1)]
         [String]$Password,
@@ -43,26 +76,39 @@
         [Alias("ResetAtLogon")]
         [Switch]$ChangePasswordAtLogon
     )
-    $ErrorActionPreference = "Stop"
     if(!$Password)
     {
-        $restPassword = Invoke-RestMethod -Uri "https://passwd.me/api/1.0/get_password.txt?length=8"
-        Write-Debug "Getting password from external address https://passwd.me/api/1.0/get_password.txt?length=8"
-        Write-Verbose "Getting password."
-        Set-ADAccountPassword -Identity $User -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $restPassword -Force)
-        Write-Output $restPassword
+        for($i=0;$i -le $User.length-1;$i++)
+        {
+            Write-Debug "Getting password from external address https://passwd.me/api/1.0/get_password.txt?length=8"
+            $restPassword = Invoke-RestMethod -Uri "https://passwd.me/api/1.0/get_password.txt?length=8"
+            Write-Debug "Attempting to change the user password and then unlock the account."
+            Set-ADAccountPassword -Identity $User[$i] -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $restPassword -Force)
+            Unlock-ADAccount $User[$i]
+            $properties = @{'User'=$User[$i];'New Password'=$restPassword}
+            $object = New-Object -TypeName PSOBject -Property $properties
+            Write-Output $object
+        }
     }
     else
     {
-        Set-ADAccountPassword -Identity $User -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force)
+        for($i=0;$i -le $User.length-1;$i++)
+        {
+            Write-Debug "Attempting to change the user password and then unlock the account."
+            Set-ADAccountPassword -Identity $User[$i] -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force)
+            Unlock-ADAccount $User[$i]
+            $properties = @{'User'=$User[$i];'New Password'=$Password}
+            $object = New-Object -TypeName PSOBject -Property $properties
+            Write-Output $object
+        }
+
     }
     if($ChangePasswordAtLogon)
     {
-        Set-ADuser $User -ChangePasswordAtLogon $true
-        Unlock-ADAccount $User
-    }
-    else
-    {
-        Unlock-ADAccount $User
+        for($i=0;$i -le $User.length-1;$i++)
+        {
+            Write-Debug "Attempting to set the user account to change the password at logon."
+            Set-ADuser $User[$i] -ChangePasswordAtLogon $true
+        }
     }
 }
