fd9074a2766d4f534d2d640ad93b24931e6a6739
Dakota Clark <dakota@allclarks.com>
2014-10-22 07:41:12 -0700

    Removed un-needed files. Added function for consistent passwords that will work with strict AD password requirements.

--- BetterPSAD.psm1
+++ BetterPSAD.psm1
@@ -47,12 +47,12 @@
     {
         for($i=0;$i -le $User.length-1;$i++)
         {
-            Write-Debug "Getting password from external address https://passwd.me/api/1.0/get_password.txt?length=8"
-            $restPassword = Invoke-RestMethod -Uri "https://passwd.me/api/1.0/get_password.txt?length=8"
+            Write-Debug "Getting password from function New-SWRandomPassword."
+            $newPassword = New-SWRandomPassword
             Write-Debug "Attempting to change the user password and then unlock the account."
-            Set-ADAccountPassword -Identity $User[$i] -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $restPassword -Force)
+            Set-ADAccountPassword -Identity $User[$i] -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $newPassword -Force)
             Unlock-ADAccount $User[$i]
-            $properties = @{'User'=$User[$i];'New Password'=$restPassword}
+            $properties = @{'User'=$User[$i];'New Password'=$newPassword}
             $object = New-Object -TypeName PSOBject -Property $properties
             Write-Output $object
         }
@@ -137,4 +137,133 @@ by querying the event logs on the PDC emulation in the domain.
                 Select-Object -Property TimeCreated, @{Label='UserName';Expression={$_.Properties[0].Value}},`
                     @{Label='ClientName';Expression={$_.Properties[1].Value}}} -Credential (Get-Credential) | 
                         Select-Object -Property TimeCreated, 'UserName', 'ClientName'
+}
+function New-SWRandomPassword
+{
+    <#
+    .Synopsis
+       Generates one or more complex passwords designed to fulfill the requirements for Active Directory
+    .DESCRIPTION
+       Generates one or more complex passwords designed to fulfill the requirements for Active Directory
+    .EXAMPLE
+       New-SWRandomPassword
+
+       Will generate one password with a length of 8 chars.
+    .EXAMPLE
+       New-SWRandomPassword -MinPasswordLength 8 -MaxPasswordLength 12 -Count 4
+
+       Will generate four passwords with a length of between 8 and 12 chars.
+    .OUTPUTS
+       [String]
+    .NOTES
+       Written by Simon Wåhlin, blog.simonw.se
+       I take no responsibility for any issues caused by this script.
+    .FUNCTIONALITY
+       Generates random passwords
+    .LINK
+       http://blog.simonw.se/powershell-generating-random-password-for-active-directory/
+   
+    #>
+    [CmdletBinding(ConfirmImpact='Low')]
+    [OutputType([String])]
+    Param
+    (
+        # Specifies minimum password length
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=0)]
+        [ValidateScript({$_ -gt 0})]
+        [Alias("Min")] 
+        [int]$MinPasswordLength = 8,
+        
+        # Specifies maximum password length
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [ValidateScript({$_ -ge $MinPasswordLength})]
+        [Alias("Max")]
+        [int]$MaxPasswordLength = 12,
+        
+        # Specifies an array of strings containing charactergroups from which the password will be generated.
+        # At least one char from each group (string) will be used.
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=2)]
+        [String[]]$InputStrings = @('abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '0123456789', '!"#%&'),
+        
+        # Specifies number of passwords to generate.
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=3)]
+        [ValidateScript({$_ -gt 0})]
+        [int]$Count = 1
+    )
+    Begin
+    {
+        Function Get-Seed
+        {
+            # Generate a seed for future randomization
+            $RandomBytes = New-Object -TypeName 'System.Byte[]' 4
+            $Random = New-Object -TypeName 'System.Security.Cryptography.RNGCryptoServiceProvider'
+            $Random.GetBytes($RandomBytes)
+            [BitConverter]::ToInt32($RandomBytes, 0)
+        }
+    }
+    Process
+    {
+        For($iteration = 1;$iteration -le $Count; $iteration++)
+        {
+            # Create char arrays containing possible chars
+            [char[][]]$CharGroups = $InputStrings
+
+            # Set counter of used groups
+            [int[]]$UsedGroups = for($i=0;$i -lt $CharGroups.Count;$i++){0}
+
+            # Create new char-array to hold generated password
+            if($MinPasswordLength -eq $MaxPasswordLength)
+            {
+                # If password length is set, use set length
+                $password = New-Object -TypeName 'System.Char[]' $MinPasswordLength
+            }
+            else
+            {
+                # Otherwise randomize password length
+                $password = New-Object -TypeName 'System.Char[]' `
+                    (Get-Random -SetSeed $(Get-Seed) -Minimum $MinPasswordLength -Maximum $($MaxPasswordLength+1))
+            }
+
+            for($i=0;$i -lt $password.Length;$i++)
+            {
+                if($i -ge ($password.Length - ($UsedGroups | Where-Object {$_ -eq 0}).Count))
+                {
+                    # Check if number of unused groups are equal of less than remaining chars
+                    # Select first unused CharGroup
+                    $CharGroupIndex = 0
+                    while(($UsedGroups[$CharGroupIndex] -ne 0) -and ($CharGroupIndex -lt $CharGroups.Length))
+                    {
+                        $CharGroupIndex++
+                    }
+                }
+                else
+                {
+                    #Select Random Group
+                    $CharGroupIndex = Get-Random -SetSeed $(Get-Seed) -Minimum 0 -Maximum $CharGroups.Length
+                }
+
+                # Set current position in password to random char from selected group using a random seed
+                $password[$i] = Get-Random -SetSeed $(Get-Seed) -InputObject $CharGroups[$CharGroupIndex]
+                # Update count of used groups.
+                $UsedGroups[$CharGroupIndex] = $UsedGroups[$CharGroupIndex] + 1
+            }
+            Write-Output -InputObject $($password -join '')
+        }
+    }
 }
--- help.ps1
+++
@@ -1,6 +0,0 @@
-﻿$userArray = 1..3
-for($i=1;$i -le $userArray.Count+1; $i++)
-{
-    $restPassword = Invoke-RestMethod -Uri "https://passwd.me/api/1.0/get_password.txt?length=16"
-    New-Object psobject -Property @{User = $userArray[$i]; Password = $restPassword}
-}
